3bb188f486a792e1255d346a746fb39a154a53e3
lochuynhvn <huynhhuulocvn@gmail.com>
2018-03-02 11:46:20 +0700

    1st commit

---
+++ src/test/java/search/Search.java
@@ -0,0 +1,39 @@
+package search;
+import org.junit.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Search {
+	@BeforeSuite
+	public void setup() {
+		System.out.println("--------Set Up--------------");
+		this.driver = new ChromeDriver();
+		this.driver.get("https://www.google.com/");
+		
+	}
+
+	@Test
+	public void TC_001() {
+		WebElement e = this.driver.findElement(By.xpath("//input[@id='lst-ib']"));
+		e.sendKeys("Implement test case #1");
+	}
+
+	@Test
+	public void TC_002() {
+		WebElement e = this.driver.findElement(By.xpath("//input[@id='lst-ib']"));
+		e.sendKeys("Implement test case #2");
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		this.driver.quit();
+	}
+
+	WebDriver driver;
+}
